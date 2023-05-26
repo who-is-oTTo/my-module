ad9ac7f1cef13790630562ad3ef87057e992069b
Illya <112540387+who-is-oTTo@users.noreply.github.com>
2023-05-26 18:12:30 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,102 +1,50 @@
 # test_my_module.py
-import math
 import unittest
-import Calculator
-
-
-class Calculator:
-    def add(self, a, b):
-        return a + b
-
-    def subtract(self, a, b):
-        return a - b
-
-    def multiply(self, a, b):
-        return a * b
-
-    def divide(self, a, b):
-        return a / b if b != 0 else None
-
-    def square_root(self, a):
-        return math.sqrt(a) if a >= 0 else None
-
-    def power(self, a, b):
-        return a ** b
-
-    def max(self, a, b):
-        return max(a, b)
-
-    def min(self, a, b):
-        return min(a, b)
-
+import math
 
 class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = Calculator()
-        
+
+    class Calculator:
+        def add(self, a, b):
+            return a + b
+
+        def subtract(self, a, b):
+            return a - b
+
+        def multiply(self, a, b):
+            return a * b
+
+        def divide(self, a, b):
+            return a / b if b != 0 else None
+
+        def square_root(self, a):
+            return math.sqrt(a) if a >= 0 else None
+
     def test_add(self):
         result = self.calculator.add(2, 3)
         self.assertEqual(result, 5)
-        
+
     def test_subtract(self):
         result = self.calculator.subtract(5, 2)
         self.assertEqual(result, 3)
-        
+
     def test_multiply(self):
         result = self.calculator.multiply(4, 3)
         self.assertEqual(result, 12)
-        
+
     def test_divide(self):
         result = self.calculator.divide(10, 2)
         self.assertEqual(result, 5)
-        
+
     def test_divide_by_zero(self):
         result = self.calculator.divide(10, 0)
         self.assertIsNone(result)
-        
+
     def test_square_root(self):
         result = self.calculator.square_root(16)
         self.assertEqual(result, 4)
-    def test_square_root_negative_number(self):
-        result = self.calculator.square_root(-16)
-        self.assertIsNone(result)
-        
-    def test_power(self):
-        result = self.calculator.power(2, 3)
-        self.assertEqual(result, 8)
-        
-    def test_power_zero_exponent(self):
-        result = self.calculator.power(5, 0)
-        self.assertEqual(result, 1)
-        
-    def test_power_negative_exponent(self):
-        result = self.calculator.power(3, -2)
-        self.assertAlmostEqual(result, 0.111, places=3)
-        
-    def test_max(self):
-        result = self.calculator.max(7, 5)
-        self.assertEqual(result, 7)
-        
-    def test_max_negative_numbers(self):
-        result = self.calculator.max(-3, -8)
-        self.assertEqual(result, -3)
-        
-    def test_max_equal_numbers(self):
-        result = self.calculator.max(4, 4)
-        self.assertEqual(result, 4)
-        
-    def test_min(self):
-        result = self.calculator.min(7, 5)
-        self.assertEqual(result, 5)
-        
-    def test_min_negative_numbers(self):
-        result = self.calculator.min(-3, -8)
-        self.assertEqual(result, -8)
-        
-    def test_min_equal_numbers(self):
-        result = self.calculator.min(4, 4)
-        self.assertEqual(result, 4)
-
 
 if __name__ == '__main__':
     unittest.main()
