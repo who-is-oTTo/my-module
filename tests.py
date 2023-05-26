176ee6b76cd30a7a3d92e88a3febf83691c95cd2
Illya <112540387+who-is-oTTo@users.noreply.github.com>
2023-05-26 18:52:52 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -22,46 +22,34 @@ class TestCalculator(unittest.TestCase):
         self.calculator = Calculator()
 
     def test_add(self):
+        # Перевірка додавання двох чисел
         result = self.calculator.add(2, 3)
         self.assertEqual(result, 5)
 
     def test_subtract(self):
+        # Перевірка віднімання двох чисел
         result = self.calculator.subtract(5, 2)
         self.assertEqual(result, 3)
 
     def test_multiply(self):
+        # Перевірка множення двох чисел
         result = self.calculator.multiply(4, 3)
         self.assertEqual(result, 12)
 
     def test_divide(self):
+        # Перевірка ділення двох чисел
         result = self.calculator.divide(10, 2)
         self.assertEqual(result, 5)
 
     def test_divide_by_zero(self):
-        result = self.calculator.divide(10, 0)
-        self.assertIsNone(result)
+        # Перевірка ділення на нуль, очікується помилка ZeroDivisionError
+        with self.assertRaises(ZeroDivisionError):
+            self.calculator.divide(10, 0)
 
     def test_square_root(self):
+        # Перевірка обчислення квадратного кореня
         result = self.calculator.square_root(16)
         self.assertEqual(result, 4)
-
-    # Тест для перевірки наявності неіснуючого методу
-    def test_missing_method(self):
-        with self.assertRaises(AttributeError):
-            # Виклик неіснуючого методу "nonexistent_method"
-            self.calculator.nonexistent_method(2, 3)
-
-    # Тест для перевірки некоректного типу вхідних даних
-    def test_invalid_input(self):
-        with self.assertRaises(TypeError):
-            # Виклик методу "add" з рядковим аргументом
-            self.calculator.add("2", 3)
-
-    # Тест для перевірки ділення на нуль
-    def test_zero_division(self):
-        with self.assertRaises(ZeroDivisionError):
-            # Виклик методу "divide" з діленням на нуль
-            self.calculator.divide(5, 0)
 
 if __name__ == '__main__':
     unittest.main()
