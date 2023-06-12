134ce7680385d2c6254709a5250880674d63a0c3
Illya <112540387+who-is-oTTo@users.noreply.github.com>
2023-06-12 12:16:24 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -37,45 +37,6 @@ class TestCalculator(unittest.TestCase):
         result = self.calculator.divide(10, 2)
         self.assertEqual(result, 5, "Помилка у методі divide()")
 
-    def test_square_rootimport unittest
-import math
-
-class Calculator:
-    def add(self, a, b):
-        return a + b
-
-    def subtract(self, a, b):
-        return a - b
-
-    def multiply(self, a, b):
-        return a * b
-
-    def divide(self, a, b):
-        return a / b if b != 0 else None
-
-    def square_root(self, a):
-        return math.sqrt(a) if a >= 0 else None
-
-class TestCalculator(unittest.TestCase):
-    def setUp(self):
-        self.calculator = Calculator()
-
-    def test_add(self):
-        result = self.calculator.add(2, 3)
-        self.assertEqual(result, 5, "Помилка у методі add()")
-
-    def test_subtract(self):
-        result = self.calculator.subtract(5, 2)
-        self.assertEqual(result, 3, "Помилка у методі subtract()")
-
-    def test_multiply(self):
-        result = self.calculator.multiply(4, 3)
-        self.assertEqual(result, 12, "Помилка у методі multiply()")
-
-    def test_divide(self):
-        result = self.calculator.divide(10, 2)
-        self.assertEqual(result, 5, "Помилка у методі divide()")
-
     def test_square_root(self):
         result = self.calculator.square_root(16)
         self.assertEqual(result, 4, "Помилка у методі square_root()")
@@ -94,7 +55,6 @@ class TestCalculator(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
-
 
 
 
